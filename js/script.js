db827b90a3f355368f229afe0ecf4f5d0dba51e5
jomwasi <jomwasi@gmail.com>
2022-11-19 09:41:22 +0300

    add photos

--- js/script.js
+++ js/script.js
@@ -1,11 +1,46 @@
-    jQuery('h1').click(function(){
-    alert("This is a heading tag")
+$(document).ready(function(){
+  $("#design-icon").click(function(){
+    $("#design-text").toggle();
+    $("#dev-text").hide();
+    $("#product-text").hide();
   });
+});
 
-    jQuery('p').click(function(){
-    alert("This is a paragraph")
+$(document).ready(function(){
+  $("#dev-icon").click(function(){
+    $("#dev-text").toggle();
+    $("#design-text").hide();
+    $("#product-text").hide();
   });
+});
 
-    jQuery('img').click(function(){
-    alert("This is an image")
+$(document).ready(function(){
+  $("#product-icon").click(function(){
+    $("#product-text").toggle();
+    $("#design-text").hide();
+    $("#dev-text").hide();
   });
+});
+
+function validation(){
+  let name = document.getElementById('name').value;
+  let email = document.getElementById('email').value;
+  let message = document.getElementById('message').value;
+
+    if(name === "" || name == null){
+      alert("Name is required");
+      return false;
+    }
+
+    if(email === "" || email == null){
+      alert("Email is required");
+      return false;
+    }
+
+    if(message === "" || message == null){
+      alert("Message is required");
+      return false;
+    }
+    alert(name + "," + " we have received your message. Thank you for reaching out to us.")
+    return true;
+  }
